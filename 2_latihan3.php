cbf0efa474a8dd78536c0af81e4f1307199d7415
KadekJ <46430136+KadekJ@users.noreply.github.com>
2019-02-20 13:57:56 +0700

    Add files via upload

---
+++ 2_latihan3.php
@@ -0,0 +1,18 @@
+<?php
+ $a=4;
+ $b=2;
+
+ echo $a."<br/>";       //a = 4 akan tampil 4
+ $a += 1 ;              //a berisi 4+1
+ echo $a."<br/>";
+ $a -= $b;              //a berisi 4-2
+ echo $a."<br/>";
+ $a *= $b;              //a  berisi 2 x 2
+ echo $a."<br/>";
+ $a /= $b;              //a berisi 4/2
+ echo $a."<br/>";
+ $a .= $b;              //a berisi 2.2
+ echo $a."<br/>";
+ $a %= $b;              //a berisi 22 % 2
+ echo $a."<br/>";
+?>
---
+++ latihan4.php
@@ -0,0 +1,8 @@
+<?php
+    $NAMA = "Firdausa";
+    function tampil_nama (){
+        global$NAMA;
+        echo "Nama saya  : " .$NAMA;
+    }
+    tampil_nama();
+?>
